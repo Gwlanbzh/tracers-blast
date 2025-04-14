bc7046931b7e182cc1281060444b5303e58df741
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2025-04-14 17:58:56 +0200

    Implemented basics of player movements. FIXME: As of this commit, the camera pitch loops (should be clamped) and the movements are according to the world's coordinate system.

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GetComponent<Rigidbody>().freezeRotation = true;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleKeys();
+    }
+
+    void handleKeys()
+    {
+        if (Input.GetKey("w"))
+        {
+            Debug.Log("Forward");
+            // transform.Translate(Vector3.forward * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.forward * .25f, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKey("s"))
+        {
+            Debug.Log("Backward");
+            // transform.Translate(Vector3.back * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.back * .25f, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKey("a"))
+        {
+            Debug.Log("Left");
+            // transform.Translate(Vector3.left * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.left * .25f, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKey("d"))
+        {
+            Debug.Log("Right");
+            // transform.Translate(Vector3.right * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.right * .25f, ForceMode.Impulse);
+        }
+        
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("Jump");
+            // transform.Translate(Vector3.right * Time.deltaTime);
+            GetComponent<Rigidbody>().AddForce(Vector3.up * (float)5, ForceMode.Impulse);
+        }
+        
+        // mouse movement
+        float mouseSensitivity = 1000f;
+        
+        float h = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float v = - Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        Debug.LogFormat("Horizontal: {0}", h);
+        Debug.LogFormat("Vertical: {0}", v);
+
+        Transform camera = transform.Find("camera");
+        
+        /* === Garbage, dont use ===
+        
+        if (Input.GetKey("j"))
+        {
+            camera.transform.rotation = Quaternion.Euler(0.0f, 0.0f, -1.0f);
+        }
+
+        if (Input.GetKey("k"))
+        {
+            camera.transform.rotation = Quaternion.Euler(-1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey("l"))
+        {
+            camera.transform.rotation = Quaternion.Euler(1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey(";"))
+        {
+            camera.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 1.0f);
+        }
+        
+        */
+        
+        // Pitch rotation on camera
+        camera.transform.Rotate(v, 0f, 0f, Space.Self);
+        
+        // Yaw rotation on Player
+        transform.Rotate(0f, h, 0f, Space.World);
+
+        /*
+
+        camera.transform.rotate(Quaternion.Euler(v, 0, h));
+
+        */
+    }
+}

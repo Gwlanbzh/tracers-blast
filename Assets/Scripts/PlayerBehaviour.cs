2e68d698085ff93b98a63a947a6197052cd19570
Aodren Le Gloanec <aodren.legloanec@gmail.com>
2025-04-24 16:59:45 +0200

    Remade the horizontal movements

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,67 +3,94 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     private float currentPitch;
+    private float movementForce;
+    private float frictionForce;
+    private float jumpForce;
+    private float mouseSensitivity;
+
+    private Vector3 velocity;
+
+    private Rigidbody rb;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        GetComponent<Rigidbody>().freezeRotation = true;
+        rb = GetComponent<Rigidbody>();
+        
+        rb.freezeRotation = true;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        velocity = new Vector3(0f, 0f, 0f);
+        frictionForce = 8f;
+        movementForce = 1f;
+        jumpForce = 5f;
+        
         currentPitch = 0f;
+        mouseSensitivity = 1000f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         handleKeys();
     }
 
     void handleKeys()
     {
+        Vector3 wishdir = new Vector3(0f, 0f, 0f);
+        
         if (Input.GetKey("w"))
         {
-            Debug.Log("Forward");
+            // Debug.Log("Forward");
             // transform.Translate(Vector3.forward * Time.deltaTime);
-            GetComponent<Rigidbody>().AddForce(transform.forward * .25f, ForceMode.Impulse);
+            // GetComponent<Rigidbody>().AddForce(transform.forward * movementSpeed, ForceMode.Impulse);
+            wishdir += transform.forward;
         }
         
         if (Input.GetKey("s"))
         {
-            Debug.Log("Backward");
+            // Debug.Log("Backward");
             // transform.Translate(Vector3.back * Time.deltaTime);
-            GetComponent<Rigidbody>().AddForce(transform.forward * -1f * .25f, ForceMode.Impulse);
+            // GetComponent<Rigidbody>().AddForce(transform.forward * -1f * movementSpeed, ForceMode.Impulse);
+            wishdir += -transform.forward;
         }
         
         if (Input.GetKey("a"))
         {
-            Debug.Log("Left");
+            // Debug.Log("Left");
             // transform.Translate(Vector3.left * Time.deltaTime);
-            GetComponent<Rigidbody>().AddForce(transform.right * -1f * .25f, ForceMode.Impulse);
+            // GetComponent<Rigidbody>().AddForce(transform.right * -1f * movementSpeed, ForceMode.Impulse);
+            wishdir += -transform.right;
         }
         
         if (Input.GetKey("d"))
         {
-            Debug.Log("Right");
+            // Debug.Log("Right");
             // transform.Translate(Vector3.right * Time.deltaTime);
-            GetComponent<Rigidbody>().AddForce(transform.right * .25f, ForceMode.Impulse);
+            // GetComponent<Rigidbody>().AddForce(transform.right * movementSpeed, ForceMode.Impulse);
+            wishdir += transform.right;
         }
+        
+        Vector3 acceleration = wishdir.normalized * movementForce
+                             - velocity * frictionForce;
+        
+        velocity += acceleration * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + velocity);
         
         if (Input.GetKeyDown("space"))
         {
-            Debug.Log("Jump");
+            // Debug.Log("Jump");
             // transform.Translate(Vector3.right * Time.deltaTime);
-            GetComponent<Rigidbody>().AddForce(transform.up * (float)5, ForceMode.Impulse);
+            rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
         }
         
         // mouse movement
-        float mouseSensitivity = 1000f;
-        
         float h = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float v = - Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        Debug.LogFormat("Horizontal: {0}", h);
-        Debug.LogFormat("Vertical: {0}", v);
+        // Debug.LogFormat("Horizontal: {0}", h);
+        // Debug.LogFormat("Vertical: {0}", v);
 
         Transform camera = transform.Find("camera");
         
@@ -101,10 +128,23 @@ public class PlayerBehaviour : MonoBehaviour
         // Yaw rotation on Player
         transform.Rotate(0f, h, 0f, Space.World);
 
+        // Debug.Log(isOnGround() ? "Is grounded : " : "Floating");
+
         /*
 
         camera.transform.rotate(Quaternion.Euler(v, 0, h));
 
         */
+    }
+
+    /*
+     * Check if the player is standing on ground.
+     * TODO : To make this work on uneven ground (or at least kind of),
+     *        cast 4 rays in a square shape under the player
+     *        or something like this ? 
+     */
+    bool isOnGround()
+    {
+        return Physics.Raycast(transform.position + (new Vector3(0f, .1f, 0f)), -transform.up, .2f);
     }
 }
